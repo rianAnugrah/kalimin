7f8d504b760046cf1242113ef6741041f5644103
rianAnugrah <rian.te.quiero@gmail.com>
2020-06-22 19:38:44 +0700

    banting stir

---
+++ precache-manifest.920ea3fa3c18599f16ab5373d404f0a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "539497816f98e6fa1bc6777383bf1c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6356e8eadc9a49f9a4",
+    "url": "/static/css/main.a1c2a605.chunk.css"
+  },
+  {
+    "revision": "e37f5d981542723569af",
+    "url": "/static/js/2.a27d0bab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a27d0bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6356e8eadc9a49f9a4",
+    "url": "/static/js/main.60b01b0d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
